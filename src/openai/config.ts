65115b92402ff9cbe24b8230c75eb1086d3eae0e
William Johnston <will@wwj.dev>
2023-12-19 10:47:53 -0800

    adding example .env

--- src/openai/config.ts
+++ src/openai/config.ts
@@ -7,20 +7,23 @@ import { ChatOpenAI } from 'langchain/chat_models/openai';
 export const llm = new ChatOpenAI({
     openAIApiKey: config.openai.API_KEY,
     modelName: config.openai.MODEL,
+    configuration: {
+        organization: config.openai.ORGANIZATION,
+    },
 });
 
 export function getEmbeddings(modelName?: string) {
     return new OpenAIEmbeddings({
         openAIApiKey: config.openai.API_KEY,
         modelName: modelName ?? config.openai.MODEL,
+        configuration: {
+            organization: config.openai.ORGANIZATION,
+        },
     });
 }
 
-export const vectorStore = new RedisVectorStore(
-    getEmbeddings(),
-    {
-        redisClient: client,
-        indexName: config.redis.VIDEO_INDEX_NAME,
-        keyPrefix: config.redis.VIDEO_PREFIX,
-    }
-);
+export const vectorStore = new RedisVectorStore(getEmbeddings(), {
+    redisClient: client,
+    indexName: config.redis.VIDEO_INDEX_NAME,
+    keyPrefix: config.redis.VIDEO_PREFIX,
+});
