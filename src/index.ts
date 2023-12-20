1652f2746e99d72e1eb91264b00d6cc4729afea4
William Johnston <will@wwj.dev>
2023-12-20 09:52:28 -0800

    cleaning up index

--- src/index.ts
+++ src/index.ts
@@ -3,26 +3,20 @@ import * as openai from './openai/index.js';
 import * as hf from './hf/index.js';
 import * as transcripts from './transcripts/index.js';
 
+const question =
+  'I am new to Redis, I understand a little bit about using Redis for caching. However, I would like to learn more about Redis Streams. Can you help me with that?';
+
+const summarize = config.use.OPENAI ? openai.summarize : hf.summarize;
+const store = config.use.OPENAI ? openai.store : hf.store;
+const search = config.use.OPENAI ? openai.search : hf.search;
+
 try {
   const videos = await transcripts.load();
+  const summaries = await summarize.docs(videos);
+  await store(summaries);
+  const result = await search(question);
 
-  if (config.use.OPENAI) {
-    const summaries = await openai.summarize.docs(videos);
-    await openai.store(summaries);
-    const result = await openai.search(
-      'I am new to Redis, I understand a little bit about using Redis for caching. However, I would like to learn more about Redis Streams. Can you help me with that?',
-    );
-
-    console.log(result.map((r) => r.metadata.link));
-  } else if (config.use.HF) {
-    const summaries = await hf.summarize.docs(videos);
-    await hf.store(summaries);
-    const result = await hf.search(
-      'I am new to Redis, I understand a little bit about using Redis for caching. However, I would like to learn more about Redis Streams. Can you help me with that?',
-    );
-
-    console.log(result.map((r) => r.metadata.link));
-  }
+  console.log(result.map((r) => r.metadata.link));
 
   process.exit();
 } catch (e) {
