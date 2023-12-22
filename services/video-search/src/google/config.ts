0826d72bde316548ed2aaa3feacdc0e82827c2d6
William Johnston <will@wwj.dev>
2023-12-22 10:52:22 -0800

    adjusting google to do semantic similarity, adjusting config to put api prefixes in front

--- services/video-search/src/google/config.ts
+++ services/video-search/src/google/config.ts
@@ -2,17 +2,19 @@ import config from '../config.js';
 import { client } from '../db.js';
 import { RedisVectorStore } from 'langchain/vectorstores/redis';
 import { ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import { TaskType } from '@google/generative-ai';
 
 export const llm = new ChatGoogleGenerativeAI({
   apiKey: config.google.API_KEY,
   modelName: config.google.SUMMARY_MODEL,
-  maxOutputTokens: 10000,
+  maxOutputTokens: 2048,
 });
 
 export function getEmbeddings(modelName?: string) {
   return new GoogleGenerativeAIEmbeddings({
     apiKey: config.google.API_KEY,
-    modelName: modelName ?? config.google.EMBEDDING_MODEL
+    modelName: modelName ?? config.google.EMBEDDING_MODEL,
+    taskType: TaskType.SEMANTIC_SIMILARITY
   });
 }
 
